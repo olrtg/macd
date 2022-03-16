d22d358f6257d4c43f1af6cf2096fdd163339e49
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2022-03-16 14:51:13 -0300

    feat: added more rules

--- src/mapper.ts
+++ src/mapper.ts
@@ -1,16 +1,24 @@
 export const commandsMap: any = {
   dock: {
+    position: (val: 'top' | 'right' | 'bottom' | 'left' = 'bottom') =>
+      `defaults write com.apple.dock orientation -string ${val}`,
     autohide: (val = false) =>
       `defaults write com.apple.dock autohide -bool ${val}`,
+    autohide_delay: (val = 0.5) =>
+      `defaults write com.apple.dock autohide-delay -float ${val}`,
     icon_size: (val = 48) =>
       `defaults write com.apple.dock tilesize -int ${val}`,
     show_recent_apps: (val = true) =>
       `defaults write com.apple.dock show-recents -bool ${val}`,
+    minimize_animation: (val: 'genie' | 'scale' | 'suck' = 'genie') =>
+      `defaults write com.apple.dock mineffect -string ${val}`,
   },
 
   menubar: {
     autohide: (val = false) =>
       `defaults write NSGlobalDomain _HIHideMenuBar -bool ${val}`,
+    flash_time_separators: (val = false) =>
+      `defaults write com.apple.menuextra.clock FlashDateSeparators -bool ${val}`,
   },
 
   finder: {
@@ -24,6 +32,19 @@ export const commandsMap: any = {
       `defaults write com.apple.finder ShowPathbar -bool ${val}`,
     pathbar_starts_at_home: (val = false) =>
       `defaults write com.apple.finder PathBarRootAtHome -bool ${val}`,
+  },
+
+  screenshots: {
+    disable_shadows: (val = false) =>
+      `defaults write com.apple.screencapture disable-shadow -bool ${val}`,
+    include_date: (val = true) =>
+      `defaults write com.apple.screencapture include-date -bool ${val}`,
+    location: (val = '~/Desktop') =>
+      `defaults write com.apple.screencapture location -string ${val}`,
+    display_thumbnail: (val = true) =>
+      `defaults write com.apple.screencapture show-thumbnail -bool ${val}`,
+    output_format: (val: 'png' | 'jpg' = 'png') =>
+      `defaults write com.apple.screencapture type -string ${val}`,
   },
 }
 
